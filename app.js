66c69210f7b5470e7f46e0a695268ec85acb69a1
architg98 <archit.g1998@gmail.com>
2022-03-18 19:33:45 +0530

    Added content for home

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const sections = document.querySelectorAll('.section');
 const secBtns = document.querySelectorAll('.controls');
 const secBtn = document.querySelectorAll('.control');
-const allSections = document.querySelectorAll('.main-content');
+const allSections = document.querySelector('.main-content');
 
 function pageTransitions() {
-    console.log("Calling is there");
     // button click active class
     for (let i = 0; i < secBtn.length; i++) {
         secBtn[i].addEventListener('click', function() {
@@ -13,6 +12,25 @@ function pageTransitions() {
             this.className += ' active-btn';
         })
     }
+
+    //Sections Active
+    allSections.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        if (id) {
+            secBtns.forEach(btn => {
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hide other sections
+            sections.forEach(section => {
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active')
+        }
+    })
 }
 
 pageTransitions();
