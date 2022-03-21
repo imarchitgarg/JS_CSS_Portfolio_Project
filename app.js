b33492b72091bcbf3da11699ce652d0f7eaf737d
architg98 <archit.g1998@gmail.com>
2022-03-21 23:19:25 +0530

    Layout changes completed

--- app.js
+++ app.js
@@ -31,6 +31,13 @@ function pageTransitions() {
             element.classList.add('active')
         }
     })
+
+    //Toggle Theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', () => {
+        let element = document.body;
+        element.classList.toggle('light-mode');
+    })
 }
 
 pageTransitions();
